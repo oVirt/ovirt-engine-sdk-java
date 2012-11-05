3e73e186f12e87acc1670e09cd7db954461c85ad
Michael Pasternak <mpastern@redhat.com>
2012-11-05 09:16:24 +0200

    Initial Import

---
+++ src/main/java/org/ovirt/engine/sdk/Api.java
@@ -0,0 +1,57 @@
+//
+// Copyright (c) 2012 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//           http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package org.ovirt.engine.sdk;
+
+import java.net.MalformedURLException;
+import org.ovirt.engine.sdk.web.ConnectionsPool;
+import org.ovirt.engine.sdk.utils.ConnectionsPoolBuilder;
+
+public class Api {
+
+    private ConnectionsPool connectionsPool = null;
+
+    public Api(String url, String username, String password) throws MalformedURLException {
+        this.connectionsPool = new ConnectionsPoolBuilder(url, username, password)
+            .build();
+    }
+
+    public Api(String url, String username, String password, boolean insecure) throws MalformedURLException {
+        this.connectionsPool = new ConnectionsPoolBuilder(url, username, password)
+            .insecure(insecure)
+            .build();
+    }
+
+    public Api(String url, String username, String password, String ca_file, boolean filter) throws MalformedURLException {
+        this.connectionsPool = new ConnectionsPoolBuilder(url, username, password)
+            .ca_file(ca_file).filter(filter)
+            .build();
+    }
+
+    public Api(String url, String username, String password, String key_file,
+              String cert_file, String ca_file, Integer port, Integer timeout,
+              Boolean persistent_auth, Boolean insecure, Boolean filter, Boolean debug) throws MalformedURLException {
+        this.connectionsPool = new ConnectionsPoolBuilder(url, username, password)
+            .key_file(key_file).cert_file(cert_file).ca_file(ca_file)
+            .port(port).timeout(timeout).persistent_auth(persistent_auth)
+            .insecure(insecure).filter(filter).debug(debug)
+            .build();
+    }
+
+    public ConnectionsPool getConnectionsPool() {
+        return this.connectionsPool;
+    }
+}
---
+++ src/main/java/org/ovirt/engine/sdk/utils/ConnectionsPoolBuilder.java
@@ -0,0 +1,199 @@
+//
+// Copyright (c) 2012 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//           http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package org.ovirt.engine.sdk.utils;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.ovirt.engine.sdk.web.ConnectionsPool;
+
+public class ConnectionsPoolBuilder {
+
+    private static int MAX_CONNECTIONS = 20;
+    private static int MAX_CONNECTIONS_PER_HOST = 50;
+    private static int MAX_CONNECTIONS_PER_ROUTE = 5;
+
+    private static String HTTP_PROTOCOL = "http";
+    private static String HTTPS_PROTOCOL = "https";
+
+    private String url;
+    private String username;
+    private String password;
+    private String key_file;
+    private String cert_file;
+    private String ca_file;
+    private int port = -1;
+    private int timeout = -1;
+    private boolean persistent_auth = true;
+    private boolean insecure = false;
+    private boolean filter = false;
+    private boolean debug = false;
+
+    private URL urlobj = null;
+
+    public ConnectionsPoolBuilder(String url, String username, String password) throws MalformedURLException {
+        url(url);
+        username(username);
+        password(password);
+    }
+
+    private ConnectionsPoolBuilder url(String url) throws MalformedURLException {
+        this.url = url;
+        this.urlobj = createURL(url);
+        return this;
+    }
+
+    private ConnectionsPoolBuilder username(String username) {
+        this.username = username;
+        return this;
+    }
+
+    private ConnectionsPoolBuilder password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public ConnectionsPoolBuilder key_file(String key_file) {
+        this.key_file = key_file;
+        return this;
+    }
+
+    public ConnectionsPoolBuilder cert_file(String cert_file) {
+        this.cert_file = cert_file;
+        return this;
+    }
+
+    public ConnectionsPoolBuilder ca_file(String ca_file) {
+        this.ca_file = ca_file;
+        return this;
+    }
+
+    public ConnectionsPoolBuilder port(int port) {
+        this.port = port;
+        return this;
+    }
+
+    public ConnectionsPoolBuilder timeout(int timeout) {
+        this.timeout = timeout;
+        return this;
+    }
+
+    public ConnectionsPoolBuilder persistent_auth(boolean persistent_auth) {
+        this.persistent_auth = persistent_auth;
+        return this;
+    }
+
+    public ConnectionsPoolBuilder insecure(boolean insecure) {
+        this.insecure = insecure;
+        return this;
+    }
+
+    public ConnectionsPoolBuilder filter(boolean filter) {
+        this.filter = filter;
+        return this;
+    }
+
+    public ConnectionsPoolBuilder debug(boolean debug) {
+        this.debug = debug;
+        return this;
+    }
+
+    private DefaultHttpClient createDefaultHttpClient(String url, String username, String password, String key_file,
+            String cert_file, String ca_file, Integer port, Integer timeout,
+            Boolean persistent_auth, Boolean insecure, Boolean filter, Boolean debug) {
+
+        DefaultHttpClient client =
+                new DefaultHttpClient(createPoolingClientConnectionManager(url, port));
+
+        client.getCredentialsProvider().setCredentials(
+                new AuthScope(getHost(url), getPort(url, port)),
+                new UsernamePasswordCredentials(username, password));
+
+        // FIXME: use all params
+
+        return client;
+    }
+
+    private ClientConnectionManager createPoolingClientConnectionManager(String url, Integer port) {
+        SchemeRegistry schemeRegistry = createSchemeRegistry(url, port);
+
+        PoolingClientConnectionManager cm =
+                new PoolingClientConnectionManager(schemeRegistry);
+        cm.setMaxTotal(MAX_CONNECTIONS);
+        cm.setDefaultMaxPerRoute(MAX_CONNECTIONS_PER_ROUTE);
+        cm.setMaxPerRoute(new HttpRoute(new HttpHost(getHost(url),
+                                                     getPort(url, port))),
+                         MAX_CONNECTIONS_PER_HOST);
+
+        return cm;
+    }
+
+    private SchemeRegistry createSchemeRegistry(String url, Integer port) {
+        SchemeRegistry schemeRegistry = new SchemeRegistry();
+        String protocol = getProtocol(url);
+        if (HTTP_PROTOCOL.equals(protocol)) {
+            schemeRegistry.register(
+                    new Scheme(HTTP_PROTOCOL,
+                            getPort(url, port),
+                            PlainSocketFactory.getSocketFactory()));
+        } else {
+            schemeRegistry.register(
+                    new Scheme(HTTPS_PROTOCOL,
+                            getPort(url, port),
+                            SSLSocketFactory.getSocketFactory()));
+        }
+
+        return schemeRegistry;
+    }
+
+    private int getPort(String url, Integer port) {
+        return port == null || port == -1 ? this.urlobj.getPort() : port;
+    }
+
+    private String getHost(String url) {
+        return this.urlobj.getHost();
+    }
+
+    private String getProtocol(String url) {
+        return this.urlobj.getProtocol();
+    }
+
+    public String getUrl() {
+        return this.url.toString();
+    }
+
+    private URL createURL(String url) throws MalformedURLException {
+        return new URL(url);
+    }
+
+    public ConnectionsPool build() {
+        return new ConnectionsPool(createDefaultHttpClient(url, username, password, key_file,
+                cert_file, ca_file, port, timeout,
+                persistent_auth, insecure, filter, debug));
+    }
+}
