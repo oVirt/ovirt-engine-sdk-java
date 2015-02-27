acd5cc888fe64a5707f3f564460bdc99b1e85d06
Juan Hernandez <juan.hernandez@redhat.com>
2015-02-27 13:16:38 +0100

    codegen: Move Java specific code to its own package
    This patch creates a new "java" package and moves all the classes that
    are specific to the Java code generation into it.
    
    Change-Id: Ic498d479972ed6dc2c213a9b5b92f18d83bb37ca
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ generator/src/main/java/org/ovirt/engine/sdk/codegen/java/templates/ResourceTemplate.java
@@ -0,0 +1,93 @@
+//
+// Copyright (c) 2012 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//           http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package org.ovirt.engine.sdk.codegen.java.templates;
+
+import org.ovirt.engine.sdk.codegen.BrokerRules;
+import org.ovirt.engine.sdk.codegen.Location;
+import org.ovirt.engine.sdk.codegen.LocationRules;
+import org.ovirt.engine.sdk.codegen.SchemaRules;
+import org.ovirt.engine.sdk.codegen.templates.AbstractTemplate;
+import org.ovirt.engine.sdk.codegen.utils.Tree;
+import org.ovirt.engine.sdk.entities.DetailedLink;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.ovirt.engine.sdk.codegen.java.utils.StringUtils.concatenateValues;
+
+public class ResourceTemplate extends AbstractTemplate {
+    public String evaluate(Tree<Location> entityTree) {
+        String brokerType = BrokerRules.getBrokerType(entityTree);
+        String entityType = SchemaRules.getSchemaType(entityTree);
+
+        // We need to store the declarations of methods and fields indexed by name in order to be able to sort them by
+        // name before generating the code, so that the resulting code will have a predictable order:
+        Map<String, String> fieldsMap = new HashMap<>();
+        Map<String, String> gettersMap = new HashMap<>();
+        Map<String, String> methodsMap = new HashMap<>();
+
+        // Find the sub-collections:
+        List<Tree<Location>> collectionTrees = entityTree.getChildren(LocationRules::isCollection);
+
+        // Generate the code for the collection fields:
+        for (Tree<Location> collectionTree : collectionTrees) {
+            String fieldType = BrokerRules.getBrokerType(collectionTree);
+            String fieldName = Character.toLowerCase(fieldType.charAt(0)) + fieldType.substring(1);
+            String fieldCode = new VariableTemplate().evaluate(fieldType, fieldName);
+            fieldsMap.put(fieldName, fieldCode);
+        }
+
+        // Generate the code for the collection getters:
+        for (Tree<Location> collectionTree : collectionTrees) {
+            String getterName = LocationRules.getName(collectionTree);
+            String getterCode = new SubCollectionGetterTemplate().evaluate(collectionTree);
+            gettersMap.put(getterName, getterCode);
+        }
+
+        // Generate the code for the methods:
+        for (DetailedLink methodLink : entityTree.get().getLinks()) {
+            String methodName = methodLink.getRel();
+            switch (methodName) {
+            case "delete":
+                String deleteMethod = new DeleteMethodTemplate().evaluate(entityTree, methodLink);
+                methodsMap.put(methodName, deleteMethod);
+                break;
+            case "update":
+                String updateMethod = new UpdateMethodTemplate().evaluate(entityTree, methodLink);
+                methodsMap.put(methodName, updateMethod);
+                break;
+            }
+        }
+
+        // Generate the code for the actions:
+        for (Tree<Location> actionTree : entityTree.getChildren(LocationRules::isAction)) {
+            String actionName = LocationRules.getName(actionTree);
+            String actionMethod = new ResourceActionMethodTemplate().evaluate(actionTree);
+            methodsMap.put(actionName, actionMethod);
+        }
+
+        // Populate the template:
+        set("broker_type", brokerType);
+        set("entity_type", entityType);
+        set("sub_collection_variables", concatenateValues(fieldsMap));
+        set("sub_collection_getters", concatenateValues(gettersMap));
+        set("methods", concatenateValues(methodsMap));
+
+        return evaluate();
+    }
+}
---
+++ generator/src/main/java/org/ovirt/engine/sdk/codegen/java/utils/UrlUtils.java
@@ -0,0 +1,59 @@
+//
+// Copyright (c) 2012 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//           http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package org.ovirt.engine.sdk.codegen.java.utils;
+
+import org.ovirt.engine.sdk.codegen.java.utils.FormatUtils;
+
+/**
+ * Provides URL related services
+ */
+public class UrlUtils {
+
+    /**
+     * Formats parameter context in to UrlParameterType member
+     * 
+     * @param context
+     *            context to format
+     * 
+     * @return UrlParameterType name
+     */
+    public static String toParamType(final String context) {
+        if (context.equals("query")) {
+            return "QUERY";
+        } else if (context.equals("matrix")) {
+            return "MATRIX";
+        }
+        throw new RuntimeException("Unsupported url parameter type \"" + context + "\".");
+    }
+
+    /**
+     * Decorates parameters names
+     * 
+     * @param parameterName
+     *            actual name
+     * 
+     * @return converted name
+     */
+    public static String toQueryParam(String parameterName) {
+        if (parameterName.equals("search")) {
+            return "query";
+        } else if (parameterName.contains("_")) {
+            return FormatUtils.toJava(parameterName);
+        }
+        return parameterName;
+    }
+}
