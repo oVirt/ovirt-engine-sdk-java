e0f712f728b0162c2eaecb1be005c114acc5b0d1
Juan Hernandez <juan.hernandez@redhat.com>
2016-04-15 08:54:37 +0200

    Build RPMs for each patch
    This patch changes the automation scripts so that the "check-patch.sh"
    script builds the RPM packages, so we make sure that no patch will break
    packaging.
    
    Change-Id: I92915f113b7159c72120b1dd6f3a661435211040
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -ex
 
+# Clean and then create the artifacts directory:
+rm -rf exported-artifacts
+mkdir -p exported-artifacts
+
 # Create a settings file that uses the our artifactory server as proxy
 # for all repositories:
 settings="$(pwd)/settings.xml"
@@ -28,6 +32,9 @@ cat > "${settings}" <<.
 # generator:
 export JAVA_HOME="$(dirname $(rpm -ql java-1.8.0-openjdk-devel | grep '/bin$'))"
 
+# Calculate the version number:
+version="$(python automation/version.py)"
+
 # Build the code generator and run it:
 mvn package -s "${settings}"
 
@@ -35,3 +42,25 @@ mvn package -s "${settings}"
 pushd sdk
   mvn package -s "${settings}"
 popd
+
+# Generate the .tar.gz file containing the generated SDK:
+tar_name="ovirt-engine-sdk-java"
+tar_prefix="${tar_name}-${version}"
+tar_file="${PWD}/${tar_prefix}.tar.gz"
+tar -czf "${tar_file}" --transform="s|^\.|${tar_prefix}|" -C sdk .
+
+# Build the RPM:
+cp "${tar_file}" packaging/.
+pushd packaging
+  export tar_version="${version}"
+  export tar_url="$(basename ${tar_file})"
+  export rpm_dist="$(rpm --eval '%dist')"
+  export rpm_release="0.0${rpm_dist}"
+  ./build.sh
+popd
+
+# Copy the RPM files to the exported artifacts directory:
+for file in "${tar_file}" $(find packaging -type f -name '*.rpm'); do
+  echo "Archiving file \"$file\"."
+  mv "$file" exported-artifacts
+done
