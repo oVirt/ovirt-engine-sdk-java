9404424e9794204b371d33901036124b71f6d144
Juan Hernandez <juan.hernandez@redhat.com>
2016-11-03 16:11:13 +0100

    Add example showing how to create a floating disk
    This patch adds an example that shows how create a disk that isn't
    attached to any virtual machine, a.k.a. floating disk.
    
    Change-Id: I08f304744bdd5f18bbf7813211692bfe7eff1de6
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ sdk/src/test/java/org/ovirt/engine/sdk4/examples/AddFloatingDisk.java
@@ -0,0 +1,74 @@
+/*
+Copyright (c) 2016 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package org.ovirt.engine.sdk4.examples;
+
+import static org.ovirt.engine.sdk4.ConnectionBuilder.connection;
+import static org.ovirt.engine.sdk4.builders.Builders.disk;
+import static org.ovirt.engine.sdk4.builders.Builders.storageDomain;
+
+import org.ovirt.engine.sdk4.Connection;
+import org.ovirt.engine.sdk4.services.DiskService;
+import org.ovirt.engine.sdk4.services.DisksService;
+import org.ovirt.engine.sdk4.types.Disk;
+import org.ovirt.engine.sdk4.types.DiskFormat;
+import org.ovirt.engine.sdk4.types.DiskStatus;
+
+// This example will connect to the server and create a new `floating` disk, one that isn't attached to any virtual
+// machine.
+public class AddFloatingDisk {
+    public static void main(String[] args) throws Exception {
+        // Create the connection to the server:
+        Connection connection = connection()
+            .url("https://engine40.example.com/ovirt-engine/api")
+            .user("admin@internal")
+            .password("redhat123")
+            .trustStoreFile("truststore.jks")
+            .build();
+
+        // Get the reference to the disks service:
+        DisksService disksService = connection.systemService().disksService();
+
+        // Add the disk:
+        Disk disk = disksService.add()
+            .disk(
+                disk()
+                .name("mydisk")
+                .description("My disk")
+                .format(DiskFormat.COW)
+                .provisionedSize(1 * (int) Math.pow(2, 20))
+                .storageDomains(
+                    storageDomain()
+                    .name("mydata")
+                )
+            )
+            .send()
+            .disk();
+
+        // Wait till the disk is completely created:
+        DiskService diskService = disksService.diskService(disk.id());
+        for (;;) {
+            Thread.sleep(5 * 1000);
+            disk = diskService.get().send().disk();
+            if (disk.status() == DiskStatus.OK) {
+                break;
+            }
+        }
+
+        // Close the connection to the server:
+        connection.close();
+    }
+}
