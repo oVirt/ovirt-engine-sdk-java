60f616b834898e1936873cc435739d24ac1fe92c
Juan Hernandez <juan.hernandez@redhat.com>
2017-04-19 10:22:51 +0200

    Add pin VM example
    This patch adds an example that shows how to modify the placement policy
    of a virtual machine so that it will always run in an specific host.
    
    Change-Id: Iedaec61abd96ca20683ba91d20662fb62bffcf44
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>
    (cherry picked from commit 6678935a9c678205f8f0092eefa73eecca5c061f)

---
+++ sdk/src/test/java/org/ovirt/engine/sdk4/examples/PinVm.java
@@ -0,0 +1,70 @@
+/*
+Copyright (c) 2017 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package org.ovirt.engine.sdk4.examples;
+
+import static org.ovirt.engine.sdk4.ConnectionBuilder.connection;
+import static org.ovirt.engine.sdk4.builders.Builders.host;
+import static org.ovirt.engine.sdk4.builders.Builders.vm;
+import static org.ovirt.engine.sdk4.builders.Builders.vmPlacementPolicy;
+
+import org.ovirt.engine.sdk4.Connection;
+import org.ovirt.engine.sdk4.services.SystemService;
+import org.ovirt.engine.sdk4.services.VmService;
+import org.ovirt.engine.sdk4.services.VmsService;
+import org.ovirt.engine.sdk4.types.Vm;
+
+// This example shows how to pin a virtual machine to a host, so that it will always run in that host.
+public class PinVm {
+    public static void main(String[] args) throws Exception {
+        // Create the connection to the server:
+        Connection connection = connection()
+            .url("https://engine40.example.com/ovirt-engine/api")
+            .user("admin@internal")
+            .password("redhat123")
+            .trustStoreFile("truststore.jks")
+            .build();
+
+        // Get the reference to the root of the tree of services:
+        SystemService systemService = connection.systemService();
+
+        // Find the virtual machine:
+        VmsService vmsService = systemService.vmsService();
+        Vm vm = vmsService.list()
+            .search("name=myvm")
+            .send()
+            .vms()
+            .get(0);
+
+        // Update the placement policy of the virtual machine so that it is pinned to the host:
+        VmService vmService = vmsService.vmService(vm.id());
+        vmService.update()
+            .vm(
+                vm()
+                .placementPolicy(
+                    vmPlacementPolicy()
+                    .hosts(
+                        host()
+                        .name("myhost")
+                    )
+                )
+            )
+            .send();
+
+        // Close the connection to the server:
+        connection.close();
+    }
+}
