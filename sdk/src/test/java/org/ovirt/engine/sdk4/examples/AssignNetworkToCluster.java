f7255d7d5e965e4b3c7284e227d8ec8af48a5202
Ondra Machacek <omachace@redhat.com>
2017-05-04 21:17:54 +0200

    Add assign logical network to cluster example
    This patch add example of how to assign logical network to cluster.
    
    Change-Id: I43868beaecc20c6d413772cf1c590c710aaef324
    Signed-off-by: Ondra Machacek <omachace@redhat.com>
    (cherry picked from commit ba54b8cce7a46e5bfe081af813f3ebe5e7bcbd89)

---
+++ sdk/src/test/java/org/ovirt/engine/sdk4/examples/AssignNetworkToCluster.java
@@ -0,0 +1,67 @@
+/*
+Copyright (c) 2017 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package org.ovirt.engine.sdk4.examples;
+
+import static org.ovirt.engine.sdk4.ConnectionBuilder.connection;
+import static org.ovirt.engine.sdk4.builders.Builders.network;
+
+import org.ovirt.engine.sdk4.Connection;
+import org.ovirt.engine.sdk4.services.AssignedNetworksService;
+import org.ovirt.engine.sdk4.services.ClusterService;
+import org.ovirt.engine.sdk4.services.ClustersService;
+import org.ovirt.engine.sdk4.services.NetworksService;
+import org.ovirt.engine.sdk4.types.Cluster;
+import org.ovirt.engine.sdk4.types.Network;
+
+// This example will connect to the server and assign 'mynetwork' network to cluster 'mycluster':
+public class AssignNetworkToCluster {
+    public static void main(String[] args) throws Exception {
+        // Create the connection to the server:
+        Connection connection = connection()
+            .url("https://engine40.example.com/ovirt-engine/api")
+            .user("admin@internal")
+            .password("redhat123")
+            .trustStoreFile("truststore.jks")
+            .build();
+
+        // Locate the networks service and use it to find the network:
+        NetworksService networksService = connection.systemService().networksService();
+        Network network = networksService.list().search(
+            "name=mynetwork and datacenter=mydatacenter"
+        ).send().networks().get(0);
+
+        // Locate the clusters service and use it to find the cluster:
+        ClustersService clustersService = connection.systemService().clustersService();
+        Cluster cluster = clustersService.list().search("name=mycluster").send().clusters().get(0);
+
+        // Locate the service that manages the networks of the cluster:
+        ClusterService clusterService = clustersService.clusterService(cluster.id());
+        AssignedNetworksService assignedNetworksService = clusterService.networksService();
+
+        // Use the "add" method to assign network to cluster:
+        assignedNetworksService.add()
+            .network(
+                network()
+                .id(network.id())
+                .required(true)
+            )
+            .send();
+
+        // Close the connection to the server:
+        connection.close();
+    }
+}
